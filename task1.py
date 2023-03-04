a63a0f78f8a500efec83b0125ee670b6403dc3d2
tasawar110 <126941328+tasawar110@users.noreply.github.com>
2023-03-04 00:43:14 -0800

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+#task 1
+#programming is a language that is used by the computer to understand the instructions that we give to them
+# It helps to complete any task related to computers
+# task 2
+# Syntax is writing style that is used in programming language
+# task 3
+# Print function is used to display the output of code that we have generated in any programming softaware
+# task 4
+# IDE is used along with python progamming to make coding easier as it provides several advanced options i.e 
+# it provides the suggestions also it markes when we are main an error
+#task 5
+print('mehak')
+print('skillvention')
+print('''hello
+        this is me
+        Tasawar''')
